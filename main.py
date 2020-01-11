d2bd4f321e91c0b62c3959e0da6ac4c164383fde
tdalesme <t.dalesme@laposte.net>
2020-01-11 15:34:15 +0100

    Ez gg

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Jan 11 14:40:27 2020
+import random
 
-@author: tdale
-"""
+class Equipement():
+    
+    def __init__(self, num):
+        self.distance = random.randrange(100)
+        self.id = num
+        
+    def affiche(self):
+        return "Je suis l'equipement " + str(self.id) + " etant a une distance " + str(self.distance) + "."
+    
+    #def send(self, equip):
 
-print("bonjour")
-print("hehe je suis sur git")
-print("tg")
+ 
+print("Combien d'equipement ?")
+nb_equip = int(input())
+tabEquip = []
+
+for i in range(nb_equip):
+    tabEquip.append(Equipement(i+1))
+    
+print(tabEquip[nb_equip-1].affiche())
+
+
