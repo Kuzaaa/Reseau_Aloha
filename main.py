787abb37f57cd6641b51a878c6208e9b52949e25
tdalesme <t.dalesme@laposte.net>
2020-01-11 16:49:12 +0100

    QUAND EST-CE QUE TU MET DES COLLISIONS DANS MA VIE KEVIN ???

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 ###############################################################################
 
 import random
+import math
 
 ###############################################################################
 #                                CLASS EQUIPEMENT                             #
@@ -30,15 +31,46 @@ class Equipement():
             self.sf_min = 12
         else:
             self.sf_min = -1
+            
+        self.T = (50*self.sf_min)/125000
         
     def affiche(self):
-        return "Je suis l'equipement " + str(self.id) + " etant a une distance " + str(self.distance) + " et ayant acces au sf " + str(self.sf_min) + "."
+        return ("Je suis l'equipement " + str(self.id) + 
+                " etant a une distance " + str(self.distance) + 
+                " et ayant acces au sf " + str(self.sf_min) + 
+                " donnant un temps d'envoie de message de " + str(self.T) +".")
+        
+    def get_sf(self):
+        return self.sf_min
     
-    #def send(self, equip):
+    def proba_coll(self):
+        self.P = 1 - (math.exp(-2 * (1/240) * dict_sf[self.sf_min] * self.T))
+    
+    def send(self, equip):
+        if(self.sf_min == -1):
+            pass
+        global dict_coll
+        if(self.sf_min == equip.sf_min):
+            test = random.uniform(0, 1)
+            if(test < self.P):
+                #print("collision")
+                dict_coll[self.sf_min] += 1
+            else:
+                #print("pas collision")
+                pass
 
 ###############################################################################
 #                                     MAIN                                    #
 ###############################################################################
+
+dict_coll = {}
+dict_coll[-1] = 0
+dict_coll[7] = 0
+dict_coll[8] = 0
+dict_coll[9] = 0
+dict_coll[10] = 0
+dict_coll[11] = 0
+dict_coll[12] = 0
 
 #On demande le nombre d'equipement
 ok = 0 #false -> not ok
@@ -80,4 +112,24 @@ for i in range(nb_equip):
 #Print de test du dernier equipement cree
 print(tabEquip[nb_equip-1].affiche())
 
+dict_sf = {}
+dict_sf[-1] = 0
+dict_sf[7] = 0
+dict_sf[8] = 0
+dict_sf[9] = 0
+dict_sf[10] = 0
+dict_sf[11] = 0
+dict_sf[12] = 0
 
+for i in range(len(tabEquip)):
+    dict_sf[tabEquip[i].get_sf()] += 1
+    
+print(dict_sf)
+
+for i in range(len(tabEquip)):
+    tabEquip[i].proba_coll()
+
+for i in range(len(tabEquip)):
+    tabEquip[i].send(tabEquip[(i+1)%nb_equip])
+    
+print(dict_coll)
