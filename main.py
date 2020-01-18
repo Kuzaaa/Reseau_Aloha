2bdf9664147f733ecb050ec0e106c0aa956b1c80
tdalesme <t.dalesme@laposte.net>
2020-01-18 18:29:50 +0100

    Parce que c'est nos commentaires ! vaireeph naissècère pck je c pa ecrir çen feaute

--- main.py
+++ main.py
@@ -14,8 +14,10 @@ import math
 class Equipement():
     
     def __init__(self, num, dist_max):
+        #Distance de l'equipement choisit aleatoirement entre 0 et la distance max
         self.distance = random.randrange(dist_max)
         self.id = num
+        #Calcule du ratio, determine le sf de l'equipement
         ratio = self.distance / dist_max
         if(ratio < 0.4):
             self.sf_min = 7
@@ -32,36 +34,36 @@ class Equipement():
         else:
             self.sf_min = -1
             
+        #Calcule du temps d'envoi d'un paquet pour cet equipement
         self.T = (50*self.sf_min)/125000
-        
-    def affiche(self):
-        return ("Je suis l'equipement " + str(self.id) + 
-                " etant a une distance " + str(self.distance) + 
-                " et ayant acces au sf " + str(self.sf_min) + 
-                " donnant un temps d'envoie de message de " + str(self.T) +".")
-        
+    
+    #Retourne le sf de l'equipement
     def get_sf(self):
         return self.sf_min
     
+    #Definit la probabilite de collision de l'equipement
     def proba_coll(self):
         self.P = 1 - (math.exp(-2 * (1/240) * dict_sf[self.sf_min] * self.T))
-        #print(self.P)
-    
+
+    #Envoi d'un paquet    
     def send(self, equip):
         if(self.sf_min == -1):
-            pass
+            print("Cette equipement n'a pas de sf disponible.")
         global dict_coll
         if(self.sf_min == equip.sf_min):
+            #Tire aleatoirement un chiffre entre 0 et 1
             test = random.uniform(0, 1)
             while(test < self.P):
-                #print("collision")
+                #Collision
                 dict_coll[self.sf_min] += 1
+                #Renvoi
                 test = random.uniform(0, 1)
 
 ###############################################################################
 #                                     MAIN                                    #
 ###############################################################################
 
+#Dictionnaire de collision
 dict_coll = {}
 dict_coll[-1] = 0
 dict_coll[7] = 0
@@ -126,6 +128,7 @@ for i in range(nb_equip):
 #Print de test du dernier equipement cree
 #print(tabEquip[nb_equip-1].affiche())
 
+#Dictionnaire de SF, nombre d'équipement dans chaque SF
 dict_sf = {}
 dict_sf[-1] = 0
 dict_sf[7] = 0
@@ -135,29 +138,35 @@ dict_sf[10] = 0
 dict_sf[11] = 0
 dict_sf[12] = 0
 
+#Remplissage du dictionnaire SF
 for i in range(len(tabEquip)):
     dict_sf[tabEquip[i].get_sf()] += 1
     
-#print(dict_sf)
-
+#Calcule de la probabilite de collision pour chaque équipement
 for i in range(len(tabEquip)):
     tabEquip[i].proba_coll()
 
+#Envoi des paquets
 for i in range(nb_send):
-    #print(i)
+    #Selection de l'emetteur et du receveur aleatoire
     sender = random.randrange(nb_equip)
     target = random.randrange(nb_equip)
     
+    #Tant que l'un des deux n'a pas de sf (-1) on retire l'emetteur
+    #Peut être une boucle infini si par malchance l'on a que des equipements sans sf
     while(tabEquip[sender].sf_min == -1):
         sender = random.randrange(nb_equip)
         
     while(tabEquip[target].sf_min == -1):
         target = random.randrange(nb_equip)
     
+    #Envoi d'un paquet
     tabEquip[sender].send(tabEquip[target])
 
+#Affiche le dictionnaire de collision
 print(dict_coll)
 
+#Calcule du taux de reussite
 nb_coll = 0
 for coll in dict_coll.values():
     nb_coll += coll
